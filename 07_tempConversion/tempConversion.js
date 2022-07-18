db53fdb23281376e46401946e8b3a0fdbede94ff
William Blair <w.blair@lancaster.ac.uk>
2022-07-18 23:07:14 +0100

    Complete exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,17 @@
-const ftoc = function() {
-
+const ftoc = function(f) {
+  return roundNumber((f - 32) * 5 / 9);
 };
 
-const ctof = function() {
-
+const ctof = function(c) {
+  return roundNumber((c * 9 / 5) + 32);
 };
+
+function roundNumber(number)
+{
+  return Math.round(number * 10) / 10;
+}
+
+
 
 // Do not edit below this line
 module.exports = {
